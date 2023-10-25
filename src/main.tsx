abfac9adce68604987a79507e8eed2159386a499
Nika Cuckiridze <nikacuckiridze47@gmail.com>
2023-10-25 22:38:14 +0400

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { Test } from '@/components';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Test />
+  </StrictMode>,
+);
